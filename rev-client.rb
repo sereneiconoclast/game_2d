d1ec3b75b35ab2d333ef45dc82785d30d814e1de
Greg Meyers <samvimes@comcast.net>
2012-12-09 16:47:25 +0000

    Create Networking class; more sophisticated design example in rev-server, rev-client

--- rev-client.rb
+++ rev-client.rb
@@ -1,27 +1,37 @@
 require 'rubygems'
 require 'rev'
 
+$LOAD_PATH << '.'
+require 'networking'
+
 HOST = 'localhost'
 PORT = 4321
 
-class EchoClientConnection < Rev::TCPSocket
+class EchoClientConnection < Networking
   def self.connect(host, port, *args)
     super
   end
 
   def on_connect
+    super
     puts "#{object_id} -- #{remote_addr}:#{remote_port} connected"
-    write "Hello, I am a client"
+    send_record :handshake => { :player_name => 'Fred' }
   end
 
   def on_close
-    puts "#{object_id} -- #{remote_addr}:#{remote_port} disconnected"
+    puts "Client disconnected"
     exit 0
   end
 
-  def on_read(data)
-    puts "Got: #{data}"
-    close
+  def on_record(hash)
+    @location = hash['location']
+    puts "Located at #{@location}"
+    if @location > 6
+      close
+    else
+      @location += 1
+      send_record :location => @location
+    end
   end
 end
 
--- rev-server.rb
+++ rev-server.rb
@@ -1,26 +1,67 @@
 require 'rubygems'
 require 'rev'
 
+$LOAD_PATH << '.'
+require 'networking'
+
 HOST = 'localhost'
 PORT = 4321
 
-class EchoServerConnection < Rev::TCPSocket
-  def on_connect
-    puts "#{object_id} -- #{remote_addr}:#{remote_port} connected"
+class EchoServerConnection < Networking
+  attr_reader :player_name
+
+  def setup(game)
+    puts "#{object_id} -- #{remote_addr}:#{remote_port} connected.  Waiting for handshake"
+    @game = game
   end
 
   def on_close
-    puts "#{object_id} -- #{remote_addr}:#{remote_port} disconnected"
+    puts "#{object_id} -- #{remote_addr}:#{remote_port} disconnected (delete #{@player_name})"
+    @game.remove_player self
   end
 
-  def on_read(data)
-    write data
-    puts "#{object_id} -- #{remote_addr}:#{remote_port} sent '#{data.chomp}'"
+  def on_record(hash)
+    if hash['handshake']
+      @player_name = hash['handshake']['player_name']
+      puts "#{object_id} -- #{remote_addr}:#{remote_port} known as #{@player_name}"
+      @game.add_player self
+      @location = 2
+      send_record :location => 2
+    else
+      @location = hash['location']
+      puts "#{@player_name} at #{@location}"
+      send_record :location => @location
+    end
   end
 end
 
-server = Rev::TCPServer.new('localhost', PORT, EchoServerConnection)
-server.attach(Rev::Loop.default)
+class Game < Rev::TimerWatcher
+  def initialize
+    super(0.5, true) # Fire event 2 times a second
+    @players = []
+    attach(Rev::Loop.default)
+    @server = Rev::TCPServer.new('localhost', PORT, EchoServerConnection) {|conn| conn.setup self }
+    @server.attach(Rev::Loop.default)
 
-puts "Echo server listening on #{HOST}:#{PORT}"
+    puts "Echo server listening on #{HOST}:#{PORT}"
+  end
+
+  def add_player(conn)
+    puts "Adding player: #{conn.player_name}"
+    @players << conn
+    list_players
+  end
+
+  def remove_player(conn)
+    puts "Removing player: #{conn.player_name}"
+    @players.delete conn
+    list_players
+  end
+
+  def list_players
+    puts "Players: #{@players.map(&:player_name).join(', ')}"
+  end
+end
+
+Game.new
 Rev::Loop.default.run
