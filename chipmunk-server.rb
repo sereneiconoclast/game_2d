ba3bb4763c8241eedc02d851498073dc2b84c2b8
Greg Meyers <samvimes@comcast.net>
2012-12-12 00:29:28 +0000

    Rename PlayerConnection -> ServerConnection and extract to its own file

--- chipmunk-server.rb
+++ chipmunk-server.rb
@@ -11,7 +11,7 @@ require 'gosu'
 
 $LOAD_PATH << '.'
 require 'chipmunk_utilities'
-require 'networking'
+require 'server_connection'
 require 'player'
 require 'star'
 
@@ -26,66 +26,6 @@ PORT = 4321
 # star without triggering a collision; an increased number of
 # Chipmunk step calls per update will effectively avoid this issue
 $SUBSTEPS = 6
-
-class PlayerConnection < Networking
-
-  def setup(game)
-    @game = game
-    @remote_addr, @remote_port = remote_addr, remote_port
-    puts "setup(): #{@remote_addr}:#{@remote_port} connected"
-  end
-
-  def answer_handshake(handshake)
-    # Copy this array since it's about to change
-    other_players = @game.get_all_players.dup
-
-    player_name = handshake['player_name']
-    @player = @game.add_player(self, player_name)
-
-    response = {
-      'you_are' => @player,
-      'add_players' => other_players,
-      'add_stars' => @game.get_all_stars
-    }
-    puts "#{@player} logs in from #{@remote_addr}:#{@remote_port}"
-    send_record response
-  end
-
-  def add_star(star)
-    send_record 'add_stars' => [ star ]
-  end
-
-  def add_player(player)
-    send_record 'add_players' => [ player ]
-  end
-
-  def delete_player(player)
-    send_record 'delete_players' => [ player.registry_id ]
-  end
-
-  def update_score(player)
-    send_record 'update_score' => { player.registry_id => player.score }
-  end
-
-  def on_close
-    puts "#{@player} -- #{@remote_addr}:#{@remote_port} disconnected"
-    @game.delete_player @player
-  end
-
-  def on_record(data)
-    if (handshake = data['handshake'])
-      answer_handshake(handshake)
-    elsif (move = data['move'])
-      @player.add_move move.to_sym
-    else
-      puts "IGNORING BAD DATA: #{data.inspect}"
-    end
-  end
-
-  def send_registry(registry)
-    send_record :registry => registry
-  end
-end
 
 class RegistryUpdater < Rev::TimerWatcher
   def initialize(game)
@@ -242,7 +182,7 @@ end
 
 game = Game.new
 
-server = Rev::TCPServer.new(HOSTNAME, PORT, PlayerConnection) {|conn| conn.setup(game) }
+server = Rev::TCPServer.new(HOSTNAME, PORT, ServerConnection) {|conn| conn.setup(game) }
 server.attach(Rev::Loop.default)
 
 puts "Rev server listening on #{HOSTNAME}:#{PORT}"
