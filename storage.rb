8386468749fe618ff56a68ddc5fc8c1ec01cb81e
Greg Meyers <samvimes@comcast.net>
2012-12-26 08:08:36 +0000

    Add simple persistent storage using JSON files created in a subdirectory of the user's home directory

---
+++ storage.rb
@@ -0,0 +1,35 @@
+require 'fileutils'
+require 'json'
+
+class Storage
+  def self.in_home_dir(name)
+    Storage.new "#{Dir.home}/#{name}"
+  end
+
+  def initialize(dir)
+    @dir = dir
+    FileUtils.mkdir_p @dir
+  end
+
+  def dir(subdir)
+    Storage.new("#{@dir}/#{subdir}")
+  end
+
+  def [](name)
+    Settings.new("#{@dir}/#{name}")
+  end
+end
+
+class Settings
+  def initialize(name)
+    @name = name
+    @values = File.exist?(name) ? JSON.parse(IO.read(name)) : {}
+  end
+
+  def save
+    File.open(@name, 'w') {|f| f.write(@values.to_json) }
+  end
+
+  def [](key); @values[key]; end
+  def []=(key, value); @values[key] = value; end
+end
