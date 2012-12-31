ecc1b890e4bc7dbb4ce998f354915758bf2a2bbf
Greg Meyers <samvimes@comcast.net>
2012-12-31 02:14:16 +0000

    Rename client and server files, and delete chipmunk_utilities.rb

--- chipmunk_utilities.rb
+++
@@ -1,8 +0,0 @@
-require 'chipmunk'
-
-# Convenience method for converting from radians to a Vec2 vector.
-class Numeric
-  def radians_to_vec2
-    CP::Vec2.new(Math::cos(self), Math::sin(self))
-  end
-end
