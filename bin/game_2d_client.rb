95605cf57d474876fd5b3d7866707a7a0bfefa8b
Greg Meyers <cmdr.samvimes@gmail.com>
2014-11-27 02:52:32 -0800

    Factor out a module GameClient
    Hoping to make it possible to test more of these methods, if I can separate them from the Gosu window.

--- bin/game_2d_client.rb
+++ bin/game_2d_client.rb
@@ -6,7 +6,7 @@ require 'game_2d/game_window'
 opts = Trollop::options do
   opt :name, "Player name", :type => :string, :required => true
   opt :hostname, "Hostname of server", :type => :string, :required => true
-  opt :port, "Port number", :default => DEFAULT_PORT
+  opt :port, "Port number", :default => GameClient::DEFAULT_PORT
   opt :key_size, "Key size in bits", :default => 1024
   opt :profile, "Turn on profiling", :type => :boolean
   opt :debug_traffic, "Debug network traffic", :type => :boolean
